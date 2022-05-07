cb27f5acd797ebdb9c5e894f6ad27786feec0db5
Joni Rämö <joni.ramo.91@gmail.com>
2022-05-07 10:37:46 +0300

    Add webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
-//vue.config.js
+const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 module.exports = {
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "Joni Rämö";
       return args;
     });
+  },
+  configureWebpack: {
+    plugins: [new BundleAnalyzer({ analyzerMode: "static" })],
   },
 };
