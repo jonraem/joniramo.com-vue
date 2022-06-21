e11cec12b82994704b0a94c7e91314be44d83996
Joni Rämö <yotara91@gmail.com>
2022-06-21 23:29:45 +0300

    Add Sanity Studio configuration

---
+++ studio/schemas/blockContent.js
@@ -0,0 +1,51 @@
+export default {
+  name: "blockContent",
+  title: "Block Content",
+  type: "array",
+  of: [
+    {
+      title: "Block",
+      type: "block",
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "H1", value: "h1" },
+        { title: "H2", value: "h2" },
+        { title: "H3", value: "h3" },
+        { title: "Quote", value: "blockquote" },
+      ],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
+      ],
+      marks: {
+        decorators: [
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
+        ],
+        annotations: [
+          {
+            title: "URL",
+            name: "link",
+            type: "object",
+            fields: [
+              {
+                title: "URL",
+                name: "href",
+                type: "url",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      type: "image",
+      options: { hotspot: true },
+    },
+
+    {
+      title: "Code block",
+      type: "code",
+    },
+  ],
+};
