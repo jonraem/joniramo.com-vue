d787558294a2e4141c1411a2764cc88498acbfa1
Joni Rämö <yotara91@gmail.com>
2024-12-12 19:52:28 +0200

    Fix ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,17 @@
-import vue from "eslint-plugin-vue";
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintJs from "@eslint/js";
+import prettierConfig from "@vue/eslint-config-prettier";
+import tsEslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default [
-  ...compat.extends(
-    "plugin:vue/vue3-essential",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier"
-  ),
+export default tsEslint.config(
+  eslintJs.configs.recommended,
+  tsEslint.configs.recommended,
+  prettierConfig,
   {
-    plugins: {
-      vue,
-      prettier,
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-
-      ecmaVersion: 2022,
-      sourceType: "commonjs",
-    },
-
     rules: {
-      "no-console": import.meta.env.NODE_ENV === "production" ? "warn" : "off",
-      "no-debugger": import.meta.env.NODE_ENV === "production" ? "warn" : "off",
+      "no-console": "off",
+      "no-debugger": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "vue/multi-word-component-names": "off",
     },
   },
-  {
-    files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
-
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-      },
-    },
-  },
-];
+);
